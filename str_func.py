0265bdcf12e4741cf1cd4f25031c777ab92be483
Aleksandr Podcherednik <startcpa1@yandex.ru>
2023-09-04 19:53:21 +0300

    create func title letters

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def outputs_capital_letters(string):
     return str.upper(string)
+
+
+def outputs_title_letters(string):
+    """the function outputs each word
+    with a capital letter"""
+    return str.title(string)

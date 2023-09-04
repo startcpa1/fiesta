9e86b0ff6d41ebd6cd7acfacf1ae7d6f10219b00
Aleksandr Podcherednik <startcpa1@yandex.ru>
2023-09-04 20:18:58 +0300

    testing

--- str_func.py
+++ str_func.py
@@ -1,9 +1,10 @@
 def outputs_capital_letters(string):
-"""func output string capital letters"""
+    """func output string
+    capital letters"""
     return str.upper(string)
 
 
 def outputs_title_letters(string):
-    """the function outputs each word
-    with a capital letter"""
+    """the function outputs
+    each word with a capital letter"""
     return str.title(string)
